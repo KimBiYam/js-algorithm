e49cc55b529b4890ab83396f110520f0420d6591
ChangHyunKim <hot9998@naver.com>
2021-09-28 00:30:37 +0900

    Pass programmers 42862 problem

--- programmers/42862-Gym-Suit/GymSuit.js
+++ programmers/42862-Gym-Suit/GymSuit.js
@@ -1,4 +1,34 @@
-function solution(n, lost, reserve) {
-  var answer = 0;
+function solution(n, losts, reserves) {
+  let answer = n - losts.length;
+
+  reserves.sort((a, b) => a - b);
+  losts.sort((a, b) => a - b);
+
+  reserves.forEach((reserve) => {
+    const lostIndex = losts.indexOf(reserve);
+    if (lostIndex >= 0) {
+      losts.splice(lostIndex, 1);
+      answer++;
+      return;
+    }
+
+    losts.forEach((lost, lostIndex) => {
+      if (reserves.includes(lost)) {
+        return;
+      }
+
+      if (lost === reserve - 1 || lost === reserve + 1) {
+        losts.splice(lostIndex, 1);
+        answer++;
+      }
+    });
+  });
+
   return answer;
 }
+
+const n = 1;
+const losts = [1];
+const reserves = [1];
+
+solution(n, losts, reserves);

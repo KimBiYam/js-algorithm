ff223d9b7b1edf24506d0370f95db09160e043f8
ChangHyunKim <hot9998@naver.com>
2021-11-01 23:47:09 +0900

    Fix baekjoon one line input template
    Changed with A+B question
    https://www.acmicpc.net/problem/1000

--- baekjoon/templates/oneLineInput.js
+++ baekjoon/templates/oneLineInput.js
@@ -1,24 +1,22 @@
-function solution(list) {
-  console.log(list);
-}
-
 const readline = require("readline");
+
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
 });
 
-let input;
-let list = [];
-rl.on("line", function (line) {
-  input = line;
+const solution = (line) => {
+  const numbers = line.split(" ");
+
+  const result = numbers.reduce((total, number) => total + Number(number), 0);
+
+  console.log(result);
+};
+
+rl.on("line", (line) => {
+  solution(line);
+
   rl.close();
-}).on("close", function () {
-  list.push(input.split(" ").map((el) => el));
-
-  list.push(input.split(" ").map((el) => parseInt(el)));
-
-  solution(list);
-
+}).on("close", () => {
   process.exit();
 });

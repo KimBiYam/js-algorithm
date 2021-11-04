03ee066f6484b2f5e35ecfff08534ef015d237a6
ChangHyunKim <hot9998@naver.com>
2021-11-04 22:42:51 +0900

    Fix baekjoon multi line template

--- baekjoon/templates/multiLineInput.js
+++ baekjoon/templates/multiLineInput.js
@@ -1,23 +1,19 @@
-function solution(list) {
-  console.log(list);
-}
-
 const readline = require("readline");
+
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
 });
 
-let input = [];
-let list = [];
-rl.on("line", function (line) {
+const solution = (input) => {
+  console.log(input);
+};
+
+const input = [];
+rl.on("line", (line) => {
   input.push(line);
-}).on("close", function () {
-  let n = parseInt(input[0]);
-
-  let list = input[1].split(" ").map((el) => parseInt(el));
-
-  solution(list);
+}).on("close", () => {
+  solution(input);
 
   process.exit();
 });

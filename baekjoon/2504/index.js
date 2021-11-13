eb3b75b4f87497da29084c62780ca49c63247075
ChangHyunKim <hot9998@naver.com>
2021-11-14 00:54:43 +0900

    Pass baekjoon 2504 problem

--- baekjoon/2504/index.js
+++ baekjoon/2504/index.js
@@ -1,3 +1,4 @@
+const { ifError } = require("assert");
 const readline = require("readline");
 
 const rl = readline.createInterface({
@@ -6,7 +7,63 @@ const rl = readline.createInterface({
 });
 
 const solution = (line) => {
-  console.log(line);
+  const brackets = [];
+  const inputBrackets = line.split("");
+
+  let sum = 0;
+  let temp = 1;
+
+  let isBroken = false;
+
+  inputBrackets.forEach((bracket, index, inputBrackets) => {
+    if (isBroken) {
+      return false;
+    }
+
+    switch (bracket) {
+      case "(":
+        brackets.push(bracket);
+        temp *= 2;
+        break;
+      case "[":
+        brackets.push(bracket);
+        temp *= 3;
+        break;
+      case ")":
+        if (!brackets.some((bracket) => bracket === "(")) {
+          sum = 0;
+          isBroken = true;
+          return;
+        }
+
+        if (inputBrackets[index - 1] === "(") {
+          sum += temp;
+        }
+
+        brackets.pop();
+        temp = temp / 2;
+        break;
+      case "]":
+        if (!brackets.some((bracket) => bracket === "[")) {
+          sum = 0;
+          isBroken = true;
+          return;
+        }
+
+        if (inputBrackets[index - 1] === "[") {
+          sum += temp;
+        }
+
+        brackets.pop();
+        temp = temp / 3;
+    }
+  });
+
+  if (brackets.length > 0) {
+    sum = 0;
+  }
+
+  console.log(sum);
 };
 
 rl.on("line", (line) => {

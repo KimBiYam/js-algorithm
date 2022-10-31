84ac0d9e310d5e2e18f9e16928635c1816e6bbd1
Chang Hyun Kim <ckdvk9998@gmail.com>
2022-11-01 00:00:20 +0900

    Solve `leetcode: 1. Two Sum` problem

---
+++ leetcode/1. Two Sum/index.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function (nums, target) {
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+      if (nums[i] + nums[j] === target) {
+        return [i, j];
+      }
+    }
+  }
+};
+
+const nums = [2, 7, 11, 15];
+const target = 9;
+
+console.log(twoSum(nums, target)); // [0,1]

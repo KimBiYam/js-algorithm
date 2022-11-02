ebfc9bd13cb43417088a41c2793a7fcbbe83195b
Chang Hyun Kim <ckdvk9998@gmail.com>
2022-11-02 23:50:54 +0900

    Solve `leetcode: 21. Merge Two Sorted Lists` problem

---
+++ leetcode/21. Merge Two Sorted Lists/index.js
@@ -0,0 +1,78 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} list1
+ * @param {ListNode} list2
+ * @return {ListNode}
+ */
+var mergeTwoLists = function (list1, list2) {
+  if (!list1) return list2;
+  if (!list2) return list1;
+
+  const head = new ListNode(-1);
+  let tail = head;
+
+  while (list1 && list2) {
+    if (list1.val < list2.val) {
+      tail.next = list1;
+      list1 = list1.next;
+    } else {
+      tail.next = list2;
+      list2 = list2.next;
+    }
+
+    tail = tail.next;
+  }
+
+  if (list1) {
+    tail.next = list1;
+  } else {
+    tail.next = list2;
+  }
+
+  return head.next;
+};
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} list1
+ * @param {ListNode} list2
+ * @return {ListNode}
+ */
+var mergeTwoLists = function (list1, list2) {
+  if (!list1) return list2;
+  if (!list2) return list1;
+
+  const head = new ListNode();
+  let tail = head;
+
+  while (list1 && list2) {
+    if (list1.val < list2.val) {
+      tail.next = list1;
+      list1 = list1.next;
+    } else {
+      tail.next = list2;
+      list2 = list2.next;
+    }
+
+    tail = tail.next;
+  }
+
+  if (list1) {
+    tail.next = list1;
+  } else {
+    tail.next = list2;
+  }
+
+  return head.next;
+};
